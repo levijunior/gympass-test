1b9e6c447e497851239ceb9074804a43a6a4e33c
levijr <levijr.santos@gmail.com>
2019-08-01 00:39:36 -0300

    Commits list - paused duo the apirate limit exceeded

--- components/CommitsList/index.js
+++ components/CommitsList/index.js
@@ -8,10 +8,18 @@ const CommitsList = props => {
   // console.log(props)
   return (
     <ul className={styles.commitsList}>
+      {console.log(props.commits)}
       {props.commits.map( item => (
         <Card key={item.sha}>
           <li>
-              {item.commit.message}
+            <div className={styles.container_left}>
+              <p className={styles.p_big}>
+                {item.commit.message}
+              </p>
+              <p className={styles.p_small}>
+                <b>{item.author.login}</b> authored and <b>{item.committer.login}</b> committed on {item.commit.author.date}
+              </p>
+            </div>
           </li>
         </Card>	
       ))}
--- store/commits.js
+++ store/commits.js
@@ -1,5 +1,9 @@
 export const commitStore = {
   commits: {},
+  filter: {
+    filterQuery: "",
+    filterCommits: []
+  }
 };
   
 export const COMMITS = "COMMITS";
